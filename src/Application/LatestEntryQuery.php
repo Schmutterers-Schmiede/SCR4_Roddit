ea987d3ad450b87af473a0546899fac43f8ea3f5
Philipp Schmutterer <trashmail837857@gmail.com>
2023-06-15 15:12:59 +0200

    corrected last entry query

--- src/Application/LatestEntryQuery.php
+++ src/Application/LatestEntryQuery.php
@@ -5,14 +5,14 @@ namespace Application;
 class LatestEntryQuery
 {
     public function __construct(
-        private Interfaces\ThreadRepository $threadRepository
+        private Interfaces\EntryRepository $entryRepository
     ) {
     }
 
     public function execute(): ?\Application\EntryInfoData
     {
       
-      $resEntryData = $this->threadRepository->getLatestEntry();
+      $resEntryData = $this->entryRepository->getLatestEntry();
       if($resEntryData != null)
         $res = new \Application\EntryInfoData($resEntryData->getThreadTitle(), $resEntryData->getUserName(), $resEntryData->getTimeStamp());
       else
